9eef34cdef9b71fa9ae605594f250bf07a0ecb43
Paul R Andreini <111322899+PRAndreini@users.noreply.github.com>
2026-02-27 20:08:08 -0800

    Fixing a Bug with the Last Commit (2)
    Deleted a spurious "14" from line 14: ChessEngine14.py does not exist; instead it is called ChessEngine.py

--- ChessAI.py
+++ ChessAI.py
@@ -11,7 +11,7 @@
 """
 
 ## Importing relevant packages...
-from ChessEngine14 import GameState
+from ChessEngine import GameState
 import random
 
 ## Dictionary defines the relative values of the pieces, with pawn fixed at "1".
