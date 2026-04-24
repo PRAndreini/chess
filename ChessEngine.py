adad0a484465024bc48e4f0de8d38f91f8ae0d3d
Paul R Andreini <111322899+PRAndreini@users.noreply.github.com>
2026-04-23 17:26:33 -0700

    Minor Bug Fixes
    1. Buggy Castling Code (in ChessEngine.py, last four methods defined under class GameState):
    a. In methods "get_king_side_castling_moves" and "get_queen_side_castling_moves", a spurious "and" will allow castling even through an attack. This should be "or".
    b. In method "get_castling_moves", we duplicate our call to method "search_for_attacks", when we already take care of this in "get_king_side_castling_moves" and "get_queen_side_castling_moves". This duplicity has been eliminated.
    
    2. Error in Docstring Describing Method "search_for_attacks" (in ChessEngine.py, defined under class GameState):
    a. A simple error swapping the booleans returned that the method returns; this has been corrected, but the error was only in the docstring, not the method's code.

--- ChessEngine.py
+++ ChessEngine.py
@@ -599,7 +599,7 @@ class GameState:
     def search_for_attacks(self, r, c, enemy_c) -> bool:
         """
            Searches for enemy pieces that might threaten the square, (r, c), through which one is castling.
-            If NO such threats exist, then 'return True'; if ANY such threats exist, then 'return False'.
+            If ANY such threats exist, then 'return True'; if NO such threats exist, then 'return False'.
         """
         ally_c = "w" if enemy_c == "b" else "b"
 
@@ -1175,27 +1175,21 @@ class GameState:
     def get_castling_moves(self, r, c, moves):
         """
            Generates ALL VALID CASTLING-MOVES for a KING on square (r, c); adds these to the list of valid moves.
-            WILL NEED PARAMETER 'ally_color' for the more-advanced version with 'self.square_under_attack'...
-            viz. https://www.youtube.com/watch?v=jnHlkhYVmqM&list=PLBwF487qi8MGU81nDGaeNE1EnNEPYWKY_&t=2874s
+            New method: since we updated the king- and queen-side methods, we no longer need to search for attacks.
         """
         ## Creating a shorter variable name...
-        w = True if self.white_to_move else False
-        ec = "b" if self.white_to_move else "w"
+        w = self.white_to_move
 
-        ## Essentially, we're using 'search_for_attacks(...)' to see if we're in check!
-        if ((w and self.search_for_attacks(self.white_king_location[0], self.white_king_location[1], ec)) or
-                ((not w) and self.search_for_attacks(self.black_king_location[0], self.black_king_location[1], ec))):
-            return  ## Cannot castle out of check.
+        ## Cannot castle out of check.
+        if self.current_player_is_in_check:
+            return
 
         ## King-side castling.
-        if ((w and self.current_castling_rights.wks and not(self.search_for_attacks(r, c+1, ec))) or
-                ((not w) and self.current_castling_rights.bks)):
+        if (w and self.current_castling_rights.wks) or ((not w) and self.current_castling_rights.bks):
             self.get_king_side_castling_moves(r=r, c=c, moves=moves)
 
         ## Queen-side castling.
-        if ((w and self.current_castling_rights.wqs and
-             not(self.search_for_attacks(r, c-1, ec) or self.search_for_attacks(r, c-2, ec))) or
-                ((not w) and self.current_castling_rights.bqs)):
+        if (w and self.current_castling_rights.wqs) or ((not w) and self.current_castling_rights.bqs):
             self.get_queen_side_castling_moves(r=r, c=c, moves=moves)
 
 
@@ -1209,8 +1203,8 @@ class GameState:
         if (self.board[r][c+1] == "--") and (self.board[r][c+2] == "--"):
 
             ## Yes! They are!
-            ##  Now, are these squares free from attackers?
-            if not ((self.search_for_attacks(r, c+1, ec)) and (self.search_for_attacks(r, c+2, ec))):
+            ##  Now, are these squares free from attackers? (Use "or" keyword; "and" might mess things up!)
+            if not ((self.search_for_attacks(r, c+1, ec)) or (self.search_for_attacks(r, c+2, ec))):
 
                 ## Yes! They are! King-side castling is good-to-go!
                 moves.append(Move(start_square=(r, c), end_square=(r, c+2), b=self.board, is_castling=True))
@@ -1226,8 +1220,8 @@ class GameState:
         if (self.board[r][c-1] == "--") and (self.board[r][c-2] == "--") and (self.board[r][c-3] == "--"):
 
             ## Yes! They are!
-            ##  Now, are these squares free from attackers?
-            if not (self.search_for_attacks(r, c-1, ec) and self.search_for_attacks(r, c-2, ec)):
+            ##  Now, are these squares free from attackers? (Use "or" keyword; "and" might mess things up!)
+            if not (self.search_for_attacks(r, c-1, ec) or self.search_for_attacks(r, c-2, ec)):
 
                 ## Yes! They are! Queen-side castling is good-to-go!
                 moves.append(Move(start_square=(r, c), end_square=(r, c-2), b=self.board, is_castling=True))
